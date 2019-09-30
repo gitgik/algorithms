7cbc27f511df2b14ecf649a305b07dbed4fae801
Jee Gikera <jeegiks@gmail.com>
2019-09-30 23:59:34 +0300

    add selection sort implementation

---
+++ coding-problems/selection_sort.py
@@ -0,0 +1,17 @@
+"""Selection sort implementation."""
+
+
+def selection_sort(array):
+    current_index = 0
+    while current_index < len(array) - 1:
+        smallest_index = current_index
+        for i in range(current_index + 1, len(array)):
+            if array[smallest_index] > array[i]:
+                swap(smallest_index, i, array)
+        current_index += 1
+
+    return array
+
+
+def swap(i, j, array):
+    array[i], array[j] = array[j], array[i]
