176748adbc3bb3ab3d0f3f9118b935bb7188f1f7
Jee Gikera <jeegiks@gmail.com>
2019-10-02 23:08:57 +0300

    add O(1) space implementation of is_palindrome

--- coding-problems/is_palindrome.py
+++ coding-problems/is_palindrome.py
@@ -14,3 +14,20 @@ def is_palindrome1(string):
     for i in reversed(range(len(string))):
         reversed_char.append(string[i])
     return string == "".join(reversed_char)
+
+
+def is_palindrome_best(string):
+    """
+    Complexity:
+        O(1) space
+        O(n) time
+    """
+    left_index = 0
+    right_index = len(string) - 1
+
+    while left_index < right_index:
+        if string[left_index] != string[right_index]:
+            return False
+        left_index += 1
+        right_index -= 1
+    return True
