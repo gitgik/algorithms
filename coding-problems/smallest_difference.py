a12981afab8613e14cc260ad8f8d6f948f3ebb55
Jee Gikera <jeegiks@gmail.com>
2019-10-03 22:25:13 +0300

    add time and space complexity + documentation

--- coding-problems/smallest_difference.py
+++ coding-problems/smallest_difference.py
@@ -7,12 +7,24 @@ Find the pair of numbers in the array whose absolute difference is closest to ze
 
 
 def smallest_difference(array_one, array_two):
+    """
+    Complexity:
+        Time: O( nlog(n))  +  mlog(m) )
+        where n = length of first array, m = length of second array
+        (the nlog n comes from sorting using an optimal sorting algorithm)
+
+        Space: O(1)
+    """
+
+    # first, we sort the arrays
     array_one.sort()
     array_two.sort()
 
+    # init pointers that we'll use for each array
     idx_one = 0
     idx_two = 0
 
+    #
     current_diff = float('inf')
     smallest_diff = float('inf')
 
@@ -20,14 +32,14 @@ def smallest_difference(array_one, array_two):
         first_num = array_one[idx_one]
         second_num = array_two[idx_two]
 
-        if first_num < second_num:
-            # increment the index in first array
-            current_diff = second_num - first_num
-            idx_one += 1
+        # find absolute difference
+        current_diff = abs(first_num, second_num)
 
+        if first_num < second_num:
+            # increment the index of first array
+            idx_one += 1
         elif second_num < first_num:
-            # increment index in second array
-            current_diff = first_num - second_num
+            # increment the index of second array
             idx_two += 1
         else:
             return [first_num, second_num]
