32f645bf3f54da07af1699ff0ddfc909b291b6da
Jee Gikera <jeegiks@gmail.com>
2019-10-07 22:31:14 +0300

    add documentation for traversal solution

--- coding-problems/traverse_bst.py
+++ coding-problems/traverse_bst.py
@@ -1,8 +1,22 @@
 """
-BST Traversal.
+Given a binary tree, traverse it, adding the node values into an array,
+returning the array.
+
+
+Assume a BST is defined as follows:
+
+* The left subtree of a node contains only nodes with keys less than the node's key.
+* The right subtree of a node contains only nodes with keys >= to the node's key.
+* Both the left and right subtrees must also be binary search trees.
 """
 
+
 def in_order_traverse(tree, array):
+    """
+    Complexity: O(n) time, O(n) space
+    NOTE: If we were not storing any array, the space complexity = O(d)
+    where d=depth of the longest branch in the BST.
+    """
     if tree is not None:
         in_order_traverse(tree.left, array)
         array.append(tree.value)
@@ -11,6 +25,9 @@ def in_order_traverse(tree, array):
 
 
 def pre_order_traverse(tree, array):
+    """
+    Complexity: O(n) time, O(n) space.
+    """
     if tree is not None:
         array.append(tree.value)
         pre_order_traverse(tree.left, array)
@@ -19,6 +36,8 @@ def pre_order_traverse(tree, array):
 
 
 def post_order_traverse(tree, array):
+    """
+    Complexity: O(n) time, O(n) space"""
     if tree is not None:
         post_order_traverse(tree.left, array)
         post_order_traverse(tree.right, array)
