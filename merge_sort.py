bedb7ab89d3057b0eecba1747a10322c370f3233
Jee Gikera <jeegiks@gmail.com>
2019-10-11 18:53:52 +0300

    add break statements shortcut

--- merge_sort.py
+++ merge_sort.py
@@ -27,8 +27,13 @@ def merge(left_array, right_array):
             result.append(right_array[right_index])
             right_index += 1
 
+        # if we are at the end of either of the lists,
         if left_index == len(left_array):
+            # reached the end of left list, append the remainder of right list
             result += right_array[right_index:]
+            break
         elif right_index == len(right_array):
+            # reached the end of right list, append the remainder of left list
             result += left_array[left_index:]
+            break
     return result
