d63ffb26845798210d33ae952436974916237a3d
Jee Gikera <jeegiks@gmail.com>
2019-12-18 17:53:32 +0300

    add space efficient solution to levenshtein distance problem

--- coding-problems/levenshtein_distance.py
+++ coding-problems/levenshtein_distance.py
@@ -47,3 +47,31 @@ def levenshtein_distance(str1, str2):
 
     # return the bottom right value of the two-dim array
     return edits[-1][-1]
+
+
+def levenshtein_distance_efficient(str1, str2):
+    small = str1 if len(str1) < len(str2) else str2
+    big = str1 if len(str1) >= len(str2) else str2
+
+    even_edits = [x for x in range(len(small) + 1)]
+    odd_edits = [None for x in range(len(small) + 1)]
+
+    for i in range(len(big) + 1):
+        if i % 2 == 1:
+            # current edits are odd edits
+            current_edits, previous_edits = odd_edits, even_edits
+        else:
+            # curent_edits are even edits
+            current_edits, previous_edits = even_edits, odd_edits
+
+        # for the first element, make it each equal to i
+        current_edits[0] = i
+
+        for j in range(1, len(small) + 1):
+            if big[i - 1] == small[j - 1]:
+                current_edits[j] = previous_edits[j - 1]
+            else:
+                current_edits[j] = 1 + min(
+                    previous_edits[j - 1], previous_edits[j], current_edits[j - 1]
+                )
+    return even_edits[-1] if len(big) % 2 == 0 else odd_edits[-1]
