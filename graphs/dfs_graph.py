2544cae779b00d4ac3940460056cef7b9076cae3
Jee Gikera <jeegiks@gmail.com>
2020-11-12 20:54:26 +0300

    put all dfs stuff under one roof

--- graphs/dfs_graph.py
+++ graphs/dfs_graph.py
@@ -30,3 +30,37 @@ def dfs_iterative(graph, start):
             visited.add(vertex)
             stack.extend(graph[vertex] - visited)
     return visited
+
+
+# Class implementation of a graph
+class Graph():
+    def __init__(self, name):
+        self.name = name
+        self.children = []
+
+    def add_child(self, name):
+        self.children.append(Graph(name))
+        return self
+
+    def depth_first_search(self, array):
+        """
+        Approach: Recursively call each child starting from the root vertex.
+        This allows the function to traverse the tree,
+        visiting the left-most child first and adding the values to the array
+
+        Complexity: O(V + E) time | O(V) space | V = vertices, E = edges
+        """
+
+        array.append(self.name)
+        for child in self.children:
+            # self = child
+            child.depth_first_search(array)
+        return array
+
+
+graph = Graph('A').add_child('B').add_child('C').add_child('Z')
+graph.children[0].add_child('D').add_child('E')
+graph.children[1].add_child('W').add_child('X')
+graph.children[2].add_child('@').add_child('$').add_child('&')
+
+print(graph.depth_first_search(array=[]))
