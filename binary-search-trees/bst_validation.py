daaed3cf8763cf55cd15b867739047694d972d1d
Jee Gikera <jeegiks@gmail.com>
2021-02-10 13:05:57 +0300

    update bst validation

--- binary-search-trees/bst_validation.py
+++ binary-search-trees/bst_validation.py
@@ -1,30 +1,29 @@
 """
 Given a binary tree, determine if it is a valid binary search tree (BST).
 
-Assume a BST is defined as follows:
+A BST is defined as follows:
 
 * The left subtree of a node contains only nodes with keys less than the node's key.
-* The right subtree of a node contains only nodes with keys >= to the node's key.
+* The right subtree of a node contains only nodes with keys greater than the node's key.
 * Both the left and right subtrees must also be binary search trees.
 """
 
+class TreeNode:
+    def __init__(key):
+        self.key = key
+        self.left = None
+        self.right = None
 
-def validate_bst(tree):
-    """
-    Recursive approach to validating a Binary Search Tree.
+        
+def is_bst(root):
+    return is_bst_helper(root, min_value=float("-inf"), max_value=float("inf"))
 
-    Complexity:
-        O(n) time, since we are traversing every node once.
-        O(d) space, where d == the depth of the binary search tree.
-        The depth of the tree is the length of the longest branch.
-    """
-    return is_valid_helper(tree, min_value=float("-inf"), max_value=float("inf"))
-
-def is_valid_helper(tree, min_value, max_value):
-    if tree is None:
+def is_bst_helper(root, min_value, max_value):
+    """ O(n) time | O(d) space, where d == the depth of the binary search tree."""
+    if root is None:
         return True
-    if tree.value < min_value or tree.value >= max_value:
+    if root.value <= min_value or root.value >= max_value:
         return False
 
-    left_is_valid = is_valid_helper(tree.left, min_value, tree.value)
-    return left_is_valid and is_valid_helper(tree.right, tree.value, max_value)
+    left_is_valid = is_bst_helper(root.left, min_value, root.key)
+    return left_is_valid and is_bst_helper(root.right, root.value, max_value)
